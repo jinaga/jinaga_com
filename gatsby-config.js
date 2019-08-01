cc3f7b1d707d063295079fd32a1d9b32b1567f74
Michael L Perry <michael@qedcode.com>
2019-08-01 16:15:00 -0400

    Changed name in manifest.

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Jinaga`,
+        short_name: `Jinaga`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
