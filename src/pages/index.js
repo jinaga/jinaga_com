f592ca362a8d7ce31a014e0f2773592193306ead
Michael L Perry <michael@qedcode.com>
2021-08-15 00:13:00 -0500

    Link to Jinaga Commits YouTube playlist.

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,6 +39,8 @@ const IndexPage = () => (
           dangerouslySetInnerHTML={{ __html: content.html }} />
       )}>
     </StaticQuery>
+    <h2>Demos</h2>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLlxnyxE021G9zzD65CCt_Y0dvDvWBadKj" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </Layout>
 )
 
