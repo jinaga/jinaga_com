7c10af5aa1ad3aedbd8c10e458071c10a32bdc42
Michael L Perry <michael@qedcode.com>
2024-01-25 17:16:53 -0600

    Replace Twitter icon with Discord

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,9 @@
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
-import { FaGithubAlt, FaTwitter, FaYoutube } from 'react-icons/fa'
+import { FaGithubAlt, FaYoutube } from 'react-icons/fa'
 import { MdEmail } from 'react-icons/md'
+import discord from '../images/discord-mark-white.svg'
 
 const iconStyle = {
   width: '20px',
@@ -33,17 +34,34 @@ const Header = () => (
       <div className="user-links">
         <a
           className="user-icon"
+          aria-label="YouTube"
           href="https://www.youtube.com/playlist?list=PLlxnyxE021G9zzD65CCt_Y0dvDvWBadKj"
         >
           <FaYoutube style={iconStyle} />
         </a>
-        <a className="user-icon" href="https://github.com/jinaga/jinaga.js">
+        <a
+          className="user-icon"
+          aria-label="GitHub"
+          href="https://github.com/jinaga/jinaga.js"
+        >
           <FaGithubAlt style={iconStyle} />
         </a>
-        <a className="user-icon" href="https://twitter.com/michaellperry">
-          <FaTwitter style={iconStyle} />
+        <a
+          className="user-icon"
+          aria-label="Discord"
+          href="https://discord.gg/Pssuk8uTke"
+        >
+          <img
+            src={discord}
+            alt="Discord"
+            style={{ width: '20px', height: '20px' }}
+          />
         </a>
-        <a className="user-icon" href="mailto:michael@qedcode.com">
+        <a
+          className="user-icon"
+          aria-label="Email"
+          href="mailto:michael@qedcode.com"
+        >
           <MdEmail style={iconStyle} />
         </a>
       </div>
