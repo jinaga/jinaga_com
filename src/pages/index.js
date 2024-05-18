5721361e5bba49c91710139e5964245692a82691
Michael L Perry <michael@qedcode.com>
2024-05-18 17:35:16 -0500

    Link to Jinaga.NET

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,66 @@
 import { graphql, Link, StaticQuery } from 'gatsby'
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 import siteLogo from '../images/site-logo.png'
 import good from '../images/good.png'
 import bad from '../images/bad.png'
 
-const IndexPage = () => (
-  <Layout
-    className="body-container"
-    head={
-      <div className="hero">
+const IndexPage = () => {
+  // Make the drop-down menu visible when the user clicks the current language.
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const handleLanguageSelect = () => {
+    window.location.href = 'https://jinaga.net';
+  };
+
+  return (
+    <Layout
+      className="body-container"
+      head={<div className="hero">
         <img src={siteLogo} alt="Jinaga" />
-        <p>Application-agnostic back end for web and mobile applications.</p>
-      </div>
-    }
-  >
-    <Seo
-      title="Jinaga"
-      keywords={[`jinaga`, `node`, `typescript`, `javascript`]}
-    />
-    <div>
-      <img src={bad} alt="Typically, all layers of an app are custom." />
-      <img
-        style={{ float: 'right' }}
-        src={good}
-        alt="In Jinaga, only the front end is."
-      />
-    </div>
-    <h2>Build Apps Faster</h2>
-    <p>
-      Typical web and mobile app architectures build domain knowledge into custom database
-      schemas, APIs, and front ends. That means you have to write the same idea
-      in three different ways. That slows you down.
-    </p>
-    <p>
-      Write your domain logic once. Jinaga provides an application-agnostic API
-      and database.
-    </p>
-    <Link
-      className="cta"
-      style={{ border: 'none' }}
-      to={'/documents/concepts/'}
+        <p>Application-agnostic back end for web and mobile applications</p>
+        <div className="language-container">
+          <button className="language" onClick={() => setMenuVisible(!menuVisible)}>
+            in JavaScript▾
+          </button>
+          <div className={`language-menu ${menuVisible ? 'visible' : ''}`}>
+            <button className="language" onClick={handleLanguageSelect}>
+              .NET
+            </button>
+          </div>
+        </div>
+      </div>}
     >
-      <div className="button-container">Learn the concepts</div>
-    </Link>
-    <StaticQuery
-      query={graphql`
+      <Seo
+        title="Jinaga"
+        keywords={[`jinaga`, `node`, `typescript`, `javascript`]} />
+      <div>
+        <img src={bad} alt="Typically, all layers of an app are custom." />
+        <img
+          style={{ float: 'right' }}
+          src={good}
+          alt="In Jinaga, only the front end is." />
+      </div>
+      <h2>Build Apps Faster</h2>
+      <p>
+        Typical web and mobile app architectures build domain knowledge into custom database
+        schemas, APIs, and front ends. That means you have to write the same idea
+        in three different ways. That slows you down.
+      </p>
+      <p>
+        Write your domain logic once. Jinaga provides an application-agnostic API
+        and database.
+      </p>
+      <Link
+        className="cta"
+        style={{ border: 'none' }}
+        to={'/documents/concepts/'}
+      >
+        <div className="button-container">Learn the concepts</div>
+      </Link>
+      <StaticQuery
+        query={graphql`
         query IndexQuery {
           content: markdownRemark(
             fields: { slug: { eq: "/posts/quick-example/" } }
@@ -55,14 +69,14 @@ const IndexPage = () => (
           }
         }
       `}
-      render={({ content }) => (
-        <div
-          className="page-content"
-          dangerouslySetInnerHTML={{ __html: content.html }}
-        />
-      )}
-    ></StaticQuery>
-  </Layout>
-)
+        render={({ content }) => (
+          <div
+            className="page-content"
+            dangerouslySetInnerHTML={{ __html: content.html }} />
+        )}
+      ></StaticQuery>
+    </Layout>
+  )
+}
 
 export default IndexPage
