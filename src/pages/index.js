913573b5b94169d19642977c571c860d178c1aea
Michael L Perry <michael@qedcode.com>
2024-11-23 09:17:20 -0600

    Remove hard-coded hook content

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,6 @@ import HookContent from '../components/HookContent'
 import Layout from '../components/layout'
 import QuickExampleContent from '../components/QuickExampleContent'
 import Seo from '../components/seo'
-import bad from '../images/bad.png'
-import good from '../images/good.png'
 import siteLogo from '../images/site-logo.png'
 
 const IndexPage = () => {
@@ -34,27 +32,10 @@ const IndexPage = () => {
         </div>
       </div>}
     >
-      <HookContent />
       <Seo
         title="Jinaga"
         keywords={[`jinaga`, `node`, `typescript`, `javascript`]} />
-      <div>
-        <img src={bad} alt="Typically, all layers of an app are custom." />
-        <img
-          style={{ float: 'right' }}
-          src={good}
-          alt="In Jinaga, only the front end is." />
-      </div>
-      <h2>Build Apps Faster</h2>
-      <p>
-        Typical web and mobile app architectures build domain knowledge into custom database
-        schemas, APIs, and front ends. That means you have to write the same idea
-        in three different ways. That slows you down.
-      </p>
-      <p>
-        Write your domain logic once. Jinaga provides an application-agnostic API
-        and database.
-      </p>
+      <HookContent />
       <Link
         className="cta"
         style={{ border: 'none' }}
